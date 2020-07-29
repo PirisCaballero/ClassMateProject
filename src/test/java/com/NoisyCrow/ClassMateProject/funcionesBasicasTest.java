444c25b45473c408f8bd1a27372027ab0aa9f950
PirisCaballero <aitorpiris@opendeusto.es>
2020-07-29 14:45:05 +0200

    Aitor Piris Caballero -Clase 'funcionesBasicas.java' y su test

---
+++ src/test/java/com/NoisyCrow/ClassMateProject/funcionesBasicasTest.java
@@ -0,0 +1,19 @@
+package com.NoisyCrow.ClassMateProject;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class funcionesBasicasTest {
+
+    funcionesBasicas fb;
+
+    @Before
+    public void initialize(){
+        fb = new funcionesBasicas();
+    }
+
+    @Test
+    public void testSuma(){
+        assertEquals(2.0, fb.suma(1.0, 1.0));
+    }
+}
